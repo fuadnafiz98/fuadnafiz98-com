717336f4eee132d64704f3192a2e4d42abd8bafe
fuadnafiz98 <fuadnafiz98@gmail.com>
2025-05-10 17:14:45 +0600

    feat: first version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,33 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fuadnafiz98.com',
+  output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
+
+  compressHTML: true,
+
+  build: {
+    inlineStylesheets: 'auto',
+  },
+
+  experimental: {
+    clientPrerender: true,
+  },
+
+  integrations: [sitemap()],
 });
